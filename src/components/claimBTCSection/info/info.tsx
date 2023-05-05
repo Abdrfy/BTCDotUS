b5738a33b5d75d3544cf4e33d88b7cae33679adb
Abdul Rafay <abdrfy@gmail.com>
2023-05-06 04:56:00 +0500

    WHAT: Beautifying touches

--- src/components/claimBTCSection/info/info.tsx
+++ src/components/claimBTCSection/info/info.tsx
@@ -1,11 +1,10 @@
 import SvgInfoIcons from '../../../images/InfoIcons';
 
 export const Info = () => {
-    const props = 'font-bold text-xl text-white';
     return (
-        <div className=" flex-col w-screen text-center">
-            <h1 className={props}>Decentralized names are secured on the Bitcoin</h1>
-            <h1 className={props}>blockchain, registered by Stacks.</h1>
+        <div className=" flex-col w-screen text-center text-sm tablet:text-xl font-bold text-white">
+            <h1>Decentralized names are secured on the Bitcoin</h1>
+            <h1>blockchain, registered by Stacks.</h1>
             <div className="flex justify-center">
                 <SvgInfoIcons />
             </div>
--- src/components/registerationStepsSection/register.tsx
+++ src/components/registerationStepsSection/register.tsx
@@ -1,3 +1,7 @@
 export const Register = () => {
-    return <h1 className="font-bold text-3xl text-white text-center">Register .BTC Domains In 3 Easy Steps</h1>;
+    return (
+        <div className="w-full flex flex-col items-center">
+            <h1 className="font-bold max-[640px]:w-[60%] text-xl tablet:text-3xl text-white text-center">Register .BTC Domains In 3 Easy Steps</h1>
+        </div>
+    );
 };
